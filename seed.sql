3cbb24586e9f81b153bc5294068c09bf2927eb3b
Rusty12321 <weston@hilgenfeld.org>
2023-04-07 13:13:30 -0500

    updated seed file

--- seed.sql
+++ seed.sql
@@ -1,3 +1,3 @@
-INSERT INTO playlist (name, artist, album) VALUES ('song 1', 'artist 1', 'album 1');
-INSERT INTO playlist (name, artist, album) VALUES ('song 2', 'artist 2', 'album 2');
-INSERT INTO playlist (name, artist, album) VALUES ('song 3', 'artist 3', 'album 3');
+INSERT INTO playlist (name, artist, album) VALUES ('New Light', 'John Mayer', 'Sob Rock');
+INSERT INTO playlist (name, artist, album) VALUES ('Find out Who Your Friends Are', 'Tracy Lawrence', 'For The Love');
+INSERT INTO playlist (name, artist, album) VALUES ('All The Words', 'Kutless', 'Strong Tower');
