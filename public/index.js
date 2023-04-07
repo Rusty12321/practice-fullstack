324f043f6ac1fcdb66afc09d80f3f465747ddec6
Rusty12321 <weston@hilgenfeld.org>
2023-04-07 12:05:48 -0500

    parsed data

--- public/index.js
+++ public/index.js
@@ -3,6 +3,6 @@ const container = document.querySelector(".container");
 fetch("/api/songs")
   .then((data) => data.json())
   .then((data) => {
-    container.innerHTML = `<p>${data}</p>`;
+    container.innerHTML = `<p>${JSON.parse(data)}</p>`;
     // console.log(data);
   });
